230e615c2c352d0a99f2cefbc858e5972087e6e4
Bruno <bruno.ribeiro96@hotmail.com.br>
2026-02-23 19:50:15 -0300

    style: change loading page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,4 @@
-<div>
+<div class="w-100 d-flex flex-column justify-content-center align-items-center">
+    <h1 class="mt-1">Site em construção</h1>
     <img src="/creating.avif" alt="Logo" />
 </div>
